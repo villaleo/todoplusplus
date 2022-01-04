00355542c128289977c49d2342d1e2c721b3a843
Leonardo Villalobos <levillalobos@csumb.edu>
2022-01-04 15:43:29 -0800

    Update CMake files and Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -15,7 +15,7 @@
 int main () {
     const std::string PROJECT_PATH ("/Users/lvill/dev/Todoplusplus/");
     std::string selection, user_name, user_date, user_category;
-    std::array<std::string, 3> user_details {selection, user_name, user_date};
+    std::array<std::string, 3> user_details { selection, user_name, user_date };
     std::multimap<std::string, Event> list;
 
     external::displayMenu ();
@@ -26,31 +26,24 @@ int main () {
         selection = external::toLower (selection);
         external::trimRight (selection);
 
-        if (selection == "ins") {
+        if (selection == "ins")
             list_ops::insertIntoList (user_details, list);
-        }
-        else if (selection == "rm") {
+        else if (selection == "rm")
             list_ops::removeFromList (user_details, list);
-        }
-        else if (selection == "vw") {
+        else if (selection == "vw")
             list_ops::displayList (list);
-        }
-        else if (selection == "sv") {
+        else if (selection == "sv")
             list_ops::saveListToFile (list, PROJECT_PATH);
-        }
-        else if (selection == "op") {
+        else if (selection == "op")
             list_ops::loadListFromFile (list, PROJECT_PATH);
-        }
         else if (selection == "q") {
             external::log ("Terminating...", 'w');
             continue;
         }
-        else if (selection == "h") {
+        else if (selection == "h")
             external::displayHelp ();
-        }
-        else { // Unknown action
+        else // Unknown action
             external::log ("Error: Unknown command. Press \'h\' for help.", 'e');
-        }
     } while (selection != "q");
 
     return EXIT_SUCCESS;
